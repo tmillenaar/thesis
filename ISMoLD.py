24867d99082e5811fe18997ddd9d3195c250cdc3
Timo <timo.m@live.nl>
2019-05-09 15:44:23 +0200

    Cleaned up code and seperated into a main program and a functions file.

--- ISMoLD.py
+++ ISMoLD.py
@@ -1,88 +1,10 @@
 ## Program ISMoLD (Imperfect Sorting Model of Linear Diffusion)
 ## Designed and created by Timo Millenaar (tmillenaar@gmail.com)
 
-def setNodes(i, k, newHeight, column, dt, dx, dy, rho0):
-    
-    if (newHeight[i] >= column["height"]):  ## Deposition
-        dispose_density = ( newHeight[i] - column["height"] ) * dx
-        j = math.floor( column["height"] )
-        
-        for j in range (math.floor( column["height"] ), math.ceil( newHeight[i] )):
-            if (j == math.floor( column["height"]) ):
-                if (j==math.ceil( newHeight[i] )-1): ## In the usual case that the first j is also the last
-                    dh = newHeight[i] - column["height"]
-                else:
-                    dh = math.ceil( column["height"]) - column["height"]
-            else:
-                if (j==math.ceil( newHeight[i] )-1): ## If current j is final j
-                    dh = newHeight[i]-j
-                else:
-                    dh = dy
-            
-            try:
-                current_box_density = dy * column["nodes"][j]["density"]
-            except:
-                current_box_density = 0
-                
-            if (j==math.ceil( newHeight[i] )-1): ## Final j
-                if (current_box_density+ rho0 *(dh) >= rho0):
-                    column["nodes"].update({j-1:{"density":rho0}})
-                    remainder = current_box_density+ rho0 *(dh) -rho0
-                    #print(1,remainder)
-                    column["nodes"].update({j:{"density":( remainder )}})
-                else:
-                    remainder = current_box_density+ rho0 *(dh)
-                    #print(2,remainder)
-                    column["nodes"].update({j:{"density":remainder}})
-            else:
-                #print('yep')
-                column["nodes"].update({j:{"density":rho0}})
-                
-    else:  ## Erosion
-        #starting_box_density = dy * column["nodes"][ math.ceil(column["height"]) ]["density"]
-        #print(math.ceil(column["height"]), math.floor(newHeight[i]))
-        #print("start")
-        for j in range(math.floor(column["height"]), math.floor(newHeight[i])-1, -1):
-            #print(j)
-            if (j == math.floor( column["height"] )):
-                if (j==math.floor( newHeight[i] )): ## In the usual case that the first j is also the last
-                    dh = column["height"] - newHeight[i]
-                else:
-                    dh = column["height"] - math.floor( column["height"] )
-            else:
-                if (j==math.floor( newHeight[i] )): ## If current j is final j
-                    dh = math.ceil( newHeight[i] ) - newHeight[i]
-                else:
-                    dh = dy
-                    
-            try:
-                current_box_density = dy * column["nodes"][j]["density"]
-            except:
-                current_box_density = 0
-                    
-            if (j==math.floor( newHeight[i] )): ## Final j
-                #if (rho0 *(dh) >= current_box_density):
-                    #try:
-                        #del column["nodes"][j]
-                    #except:
-                        #pass
-                    #remainder = rho0*( dh ) - current_box_density
-                    ##print(1, remainder, rho0*( j-newHeight[i] ), -current_box_density)
-                    #column["nodes"].update({j-1:{"density":remainder}})
-                #else:
-                remainder = current_box_density - rho0*( dh )
-                #print(2, remainder, rho0*( j-newHeight[i] ), -current_box_density)
-                column["nodes"].update({j:{"density":remainder}})
-            else:
-                try:
-                    del column["nodes"][j]
-                    #print("removed node "+str(j), math.floor( newHeight[i] ))
-                except:
-                    pass
-    return column
-
 import math
 import matplotlib.pyplot as plt
+
+from functions_for_ISMoLD import *
 
 yr2sec = 60*60*24*365.25
 dx= 1.e3       # lateral grid spacing (m)
@@ -92,7 +14,7 @@ totalHeight= list(range(imax+1))
 newHeight= list(range(imax+1))
 t= 0
 dt= 1          # time step in (yr)
-tmax= 1000*yr2sec     # max number of years
+tmax= 10000*yr2sec     # max number of years
 dtout = 10*yr2sec     # nr of years between write output
 tout = 0.          # threshold to write next output in years (increased every write action by dtout)
 tprogress = 0.
@@ -108,7 +30,7 @@ q0[2]= 0           # silt input (m2/s)
 
 rho0 = 2700
 
-totalInput= 100*dx
+totalInput= 0
 totalOutput= 0
 subsidence_rate = 0.#4e-4*dt/3 #m/yr
 maxVolumeLoss = 0
@@ -129,6 +51,7 @@ for i in range(imax+1):
 while (t < tmax):
     
     dt = 0.9*(dx*dx)/(2.e0*(max(k)))
+    q0[0] = 2.e-5*math.sin(3.1415 * t/tmax)
     #dt = min(tmax/1000, 0.9*(dx*dx)/(2.e0*(k[0])) )
     if t > tout:
         print("      "+str( (math.ceil(100000*t/tmax))/1000 )+"%", end="\r") ##Track progress
@@ -155,20 +78,22 @@ while (t < tmax):
         Dmh = k[0]
         newHeight[i]= columns[i]["height"] + ( (Dph*dt)/(dx*dx) )*( columns[i+1]["height"] - columns[i]["height"] )
         if (i==1):
-            newHeight[i]= newHeight[i] - ( (Dmh*dt)/(dx*dx) )*( columns[i]["height"] - columns[i-1]["height"]-q0[0]*dt/dx ) ##Proximal boundary condition
+            ##Consider effect of proximal boundary condition (i.e. i==0):
+            newHeight[1]= newHeight[1] - ( (Dmh*dt)/(dx*dx) )*( columns[1]["height"] - columns[0]["height"]-q0[0]*dt/dx ) 
         else:
             newHeight[i]= newHeight[i] - ( (Dmh*dt)/(dx*dx) )*( columns[i]["height"] - columns[i-1]["height"] )
         
-        if (i==70 and t==0): 
-            newHeight[70] = 100
-            for p in range(100):
-                columns[70]["nodes"].update({p:{"density":rho0}})
+        ##Initial spike test:
+        #if (i==70 and t==0): 
+            #newHeight[70] = 100
+            #for p in range(100):
+                #columns[70]["nodes"].update({p:{"density":rho0}})
             #print(columns[70])
-        columns[i] = setNodes(i, k, newHeight, columns[i], dt, dx, dy, rho0)
+        columns[i] = setNodes(i, k, newHeight[i], columns[i], dt, dx, dy, rho0)
         
     ## Set nodes in proximal boundary column:
     newHeight[0]= newHeight[0] - ( (Dph*dt)/(dx*dx) )*( columns[0]["height"]+q0[0]*dt/dx - columns[1]["height"] )
-    columns[0] = setNodes(0, k, newHeight, columns[0], dt, dx, dy, rho0)
+    columns[0] = setNodes(0, k, newHeight[0], columns[0], dt, dx, dy, rho0)
     
     ## Overwrite old the topography with the new profile:
     for i in range(0,imax):
@@ -185,8 +110,6 @@ totalDepositVolume= 0
 totalNodeVolume= 0
 x=list(range(imax+1))
 for i in range(imax+1):
-    #print(i, columns[i]["height"])
-    #columns[i]["height"]= columns[i]["height"]
     x[i]=i
     totalHeight[i]= columns[i]["height"]
     totalDepositVolume+= columns[i]["height"]*dx
@@ -197,7 +120,7 @@ for i in range(imax+1):
             pass
     
     
-print(columns[70])
+#print(columns[70])
 print("totalInput:", str(totalInput)+"m^3")
 print("totalOutput:", str(totalOutput)+"m^3")
 print("totalDepositVolume:", str(totalDepositVolume)+"m^3")
@@ -211,7 +134,7 @@ print("Node volume error in %:", str(100*(totalNodeVolume+totalOutput-totalInput
 print("")
 print("Plotting...")
 plt.plot(x, totalHeight)
-#plt.show()
+plt.show()
 
 
     
@@ -222,8 +145,3 @@ plt.plot(x, totalHeight)
 
 #columns[1]["nodes"].update({i:{"fractions":[0.3, 0.3, 0.4]}})
 
-#print(columns[1]["nodes"])
-
-
-
-#print(columns)
---
+++ functions_for_ISMoLD.py
@@ -0,0 +1,63 @@
+import math
+
+def setNodes(i, k, newHeight, column, dt, dx, dy, rho0):
+    
+    if (newHeight >= column["height"]):  ## Deposition
+        
+        for j in range (math.floor( column["height"] ), math.ceil( newHeight )):
+            if (j == math.floor( column["height"]) ): ## Start j
+                if (j==math.ceil( newHeight )-1): ## In the usual case that the first j is also the last
+                    dh = newHeight - column["height"]
+                else:
+                    dh = math.ceil( column["height"]) - column["height"]
+            else:
+                if (j==math.ceil( newHeight )-1): ## If current j is final j
+                    dh = newHeight-j
+                else:
+                    dh = dy ## not nesecary actually, but this is effectively the case when a whole node is set from 0 to rho0
+            
+            try:
+                current_box_density = dy * column["nodes"][j]["density"]
+            except:
+                current_box_density = 0
+                
+            if (j==math.ceil( newHeight )-1): ## Final j
+                if (current_box_density+ rho0 *(dh) >= rho0): ##Final height change exeeds one node boundary:
+                    column["nodes"].update({j-1:{"density":rho0}})
+                    remainder = current_box_density+ rho0 *(dh) -rho0
+                    column["nodes"].update({j:{"density":( remainder )}})
+                else: ##Height change is within the same node:
+                    remainder = current_box_density+ rho0 *(dh)
+                    column["nodes"].update({j:{"density":remainder}})
+            else: ##Fill whole node from 0 to full:
+                column["nodes"].update({j:{"density":rho0}})
+                
+    else:  ## Erosion
+
+        for j in range(math.floor(column["height"]), math.floor(newHeight)-1, -1): ## loop from top down
+            
+            if (j == math.floor( column["height"] )): ## Start j
+                if (j==math.floor( newHeight )): ## In the usual case that the first j is also the last
+                    dh = column["height"] - newHeight
+                else:
+                    dh = column["height"] - math.floor( column["height"] )
+            else:
+                if (j==math.floor( newHeight )): ## If current j is final j
+                    dh = math.ceil( newHeight ) - newHeight
+                else:
+                    dh = dy
+                    
+            try:
+                current_box_density = dy * column["nodes"][j]["density"]
+            except:
+                current_box_density = 0
+                    
+            if (j==math.floor( newHeight )): ## Final j
+                remainder = current_box_density - rho0*( dh )
+                column["nodes"].update({j:{"density":remainder}})
+            else: ## Node is above the newHeight thus is to be eroded
+                try:
+                    del column["nodes"][j] 
+                except:
+                    pass
+    return column
